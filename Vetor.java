d1ceebe362ccc36f58bf69469fab5f7c75cecce6
miwagrr <mariabsudre@gmail.com>
2025-12-04 14:06:17 -0300

    implementando vetor e suas operações

---
+++ Vetor.java
@@ -0,0 +1,51 @@
+public class Vetor {
+    private int[] elementos;
+    private int tamanho;
+
+    public Vetor(int capacidade){
+        this.elementos = new int[capacidade];
+        this.tamanho = 0;
+    }
+
+    public void inserirVetor(int valor) {
+        if(this.tamanho < this.elementos.length){
+            elementos[tamanho] = valor;
+            tamanho++;
+        }
+    }
+
+    public boolean buscaSequencial(int valorBuscado){
+        for(int i = 0; i < this.tamanho; i++){
+            if(this.elementos[i] == valorBuscado){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean buscaBinaria(int valorBuscado){
+        int inicio = 0;
+        int fim = this.tamanho - 1;
+
+        while(inicio <= fim){
+            int meio = (inicio + fim)/2;
+
+            if (this.elementos[meio] == valorBuscado) {
+                return true;
+            } else if(this.elementos[meio] < valorBuscado) {
+                inicio = meio + 1;
+            } else {
+                fim = meio -1;
+            }
+        }
+        return false;
+    }
+
+        public int[] getElementos() {
+        return this.elementos;
+    }
+    
+    public int getTamanho() {
+        return this.tamanho;
+    }
+}
